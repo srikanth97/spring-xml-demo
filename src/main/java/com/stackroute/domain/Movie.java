b5b2c33fd2311782d8c9d09e038cabbd47b38e90
srikanth97 <nksr.1997@gmail.com>
2019-10-01 17:00:30 +0530

    auto-wire

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,17 +1,19 @@
 package com.stackroute.domain;
 
-public class Movie {
-    private Actor actor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-    public Movie() {
-    }
+public class Movie {
+
+    private Actor actor;
 
     public Movie(Actor actor) {
         this.actor = actor;
     }
-
-    //    public void setActor(Actor actor) {
-//        this.actor = actor;
+    //    @Autowired
+//    @Qualifier("actorObject1")
+//    public void setActor(Actor actor) {
+//       this.actor = actor;
 //    }
 
     @Override
